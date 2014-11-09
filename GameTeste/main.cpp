1ef1978607dd2e1b3d1e81f7c637839dc2ab633e
Luiz Carvalho <projetodc@gmail.com>
2014-11-09 12:10:53 -0200

    Initial Commit

---
+++ GameTeste/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  GameTeste
+//
+//  Created by Luiz Carvalho on 09/11/14.
+//  Copyright (c) 2014 Resource. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
